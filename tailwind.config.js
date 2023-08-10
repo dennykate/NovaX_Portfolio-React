c27532fe4c028a22dfef9d2a97167f4a8a6433c3
Denny_Kate <dennykate22@gmail.com>
2023-08-10 21:07:54 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "open-sans": ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "peal-blue": "#F2F6FB",
+        "light-dark-blue": "#E1F0FF",
+        "golden-yellow": "#FBC108",
+        "dark-blue": "#082E53",
+      },
+    },
+  },
+  plugins: [],
+};
